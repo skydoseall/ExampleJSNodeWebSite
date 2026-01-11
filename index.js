1c4a0fa2f9fb3eb4d42ecaeca717113e42e4e381
skydoseall <skydoesall@gmail.com>
2026-01-10 16:22:17 -0800

    initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+console.log('Hello, World!');
+console.log('This is the index.js file.');
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello, World!</h1>');
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
